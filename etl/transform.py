bd195ba8df9a84f74358a989a94795cb9b11453c
Liam Roh <liam.roh@stocktonca.gov>
2025-04-24 17:11:47 -0700

    adds HR table transformation

--- etl/transform.py
+++ etl/transform.py
@@ -21,6 +21,11 @@ def transform_data(file, df):
     # Set tablename to be filename
     tablename = file.split('.')[0].replace(' ', '_')
 
+    if tablename == 'Separated_Employees_Within_30_Days':
+        df.columns = df.iloc[2]
+        df = df.drop([0, 1, 2]).reset_index(drop=True)
+        df = df.dropna(axis=1, how='all')
+
     if tablename == 'BATS-YTD':
         df['Case Status'] = df['Case Status'].fillna('Investigation Open')
 
